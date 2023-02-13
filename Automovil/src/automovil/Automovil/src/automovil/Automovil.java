6af02f9221348221be9c3638484c84f51008fb2e
jazmin08ibieta <72178556+jazmin08ibieta@users.noreply.github.com>
2023-02-13 11:19:30 -0600

    Add files via upload

---
+++ Automovil/src/automovil/Automovil/src/automovil/Automovil.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package automovil;
+
+/**
+ *
+ * @author ibiet
+ */
+public class Automovil {
+
+    private String marca;
+    private int submarca;
+    private int anio;
+    private int kilometraje;
+    private String transmision;
+    
+    public Automovil() {   
+    }
+    public Automovil(String marca, int submarca, int anio, int kilometraje, String transmision){
+        this.marca = marca;
+        this.submarca = submarca;
+        this.anio = anio;
+        this.kilometraje = kilometraje;
+        this.transmision = transmision;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getSubmarca() {
+        return submarca;
+    }
+
+    public void setSubmarca(int submarca) {
+        this.submarca = submarca;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public int getKilometraje() {
+        return kilometraje;
+    }
+
+    public void setKilometraje(int kilometraje) {
+        this.kilometraje = kilometraje;
+    }
+
+    public String getTransmision() {
+        return transmision;
+    }
+
+    public void setTransmision(String transmision) {
+        this.transmision = transmision;
+    }
+
+    @Override
+    public String toString() {
+        return "Automovil{" + "marca=" + marca + ", submarca=" + submarca + ", anio=" + anio + ", kilometraje=" + kilometraje + ", transmision=" + transmision + '}';
+    }
+    
+    
+     public void avanzar(int kilometraje){
+          System.out.println("El auto "+this.marca+ " Esta avanzando a " +kilometraje+ "kms" );
+        }
+        
+        public void frenar(String Marca){
+            System.out.print("El Automovil "+Marca+" ha frenado");
+        }
+    
+    
+}
---
+++ Automovil/src/automovil/Automovil/src/automovil/AutomovilMain.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package automovil;
+ import automovil.Automovil;
+/**
+ *
+ * @author ibiet
+ */
+public class AutomovilMain {
+   
+
+      public static void main(String[] args) {
+        //prueba de herencia clase persona.
+        
+        Automovil automovil = new Automovil("Honda", 1033, 2023, 1000, "Automatica");
+        
+        System.out.println(automovil.toString());
+        
+        
+        automovil.avanzar(27);
+        automovil.frenar("Honda");
+    }
+    
+}
+
